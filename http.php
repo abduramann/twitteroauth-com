7281f88e036a37da2320ae8b40c1571439effb16
Abduraman Blackram <39387835+abduramann@users.noreply.github.com>
2020-10-11 02:49:12 +0300

    varaibles

--- http.php
+++ http.php
@@ -4,10 +4,10 @@ define('FORCE_TLS', getenv('FORCE_TLS') === 'true');
 $httpHost = $_SERVER['HTTP_HOST'];
 $expectedHost = getenv('HOST');
 
-if (!DEBUG) {
-    if (FORCE_TLS) {
-        header("Strict-Transport-Security: max-age=31415926; includeSubDomains; preload");
-    }
+//if (!DEBUG) {
+//    if (FORCE_TLS) {
+//        header("Strict-Transport-Security: max-age=31415926; includeSubDomains; preload");
+//    }
 //    if (
 //        FORCE_TLS &&
 //        isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
@@ -20,4 +20,4 @@ if (!DEBUG) {
 //        header('Location: https://' . $expectedHost . $_SERVER['REQUEST_URI']);
 //        exit;
 //    }
-}
+//}
